df95e809ee7ebdc2a24ede5a595ae877630ac7f1
asambasi <asambasi@cisco.com>
2024-07-06 11:32:00 +0530

    Checking ui calls

--- src/app/commonservice.service.ts
+++ src/app/commonservice.service.ts
@@ -7,7 +7,9 @@ import { environment } from 'src/environments/environment';
 })
 export class CommonserviceService {
 
-  constructor(private http:HttpClient) { }
+  constructor(private http:HttpClient) {
+    this.callTradingview();
+   }
   private stockData = new BehaviorSubject<any>([]);
   private bankData = new BehaviorSubject<any>([]);
   private liveIndexsData = new BehaviorSubject<any>([]);
@@ -42,6 +44,95 @@ export class CommonserviceService {
   fetchIndexsData() { 
     this.http.get(`${this.importUrl}getData?indexs=true`).subscribe((res) => {
       this.liveIndexsData.next(res['data']); 
+    });
+  }
+  
+  
+  
+  
+  callTradingview() {
+   let payload =  {
+      'method': 'POST',
+      'json': true,
+      'url': 'https://scanner.tradingview.com/india/scan',
+      'body': {
+        "filter": [
+          {
+            "left": "exchange",
+            "operation": "equal",
+            "right": "NSE"
+          },
+          {
+            "left": "is_primary",
+            "operation": "equal",
+            "right": true
+          },
+          {
+            "left": "active_symbol",
+            "operation": "equal",
+            "right": true
+          }
+        ],
+        "options": {
+          "lang": "en"
+        },
+        "markets": [
+          "india"
+        ],
+        "symbols": {
+          "query": {
+            "types": []
+          },
+          "tickers": [],
+          "groups": []
+        },
+        "columns": [
+          "name",
+          "open",
+          "high",
+          "low",
+          "close",
+          "change",
+          "change_abs",
+          "volume",
+          "Value.Traded",
+          "change_from_open",
+          "change_from_open_abs",
+          "SMA20",
+          "SMA20|5",
+          "SMA50|5",
+          "SMA200",
+          "average_volume_10d_calc",
+          "average_volume_30d_calc",
+          "VWAP",
+          "sector",
+          "change_abs|5",
+          "change|5",
+          "BB.lower|1W",
+          "open|1W",
+          "high|1W",
+          "low|1W",
+          "close|1W",
+          "SMA20|1W",
+          "return_on_equity",
+          "debt_to_equity",
+          "price_earnings_ttm"
+        ],
+        "sort": {
+          "sortBy": "close",
+          "sortOrder": "desc"
+        },
+        "price_conversion": {
+          "to_symbol": false
+        },
+        "range": [
+          0, 6500
+        ]
+      }
+    }
+    
+    this.http.post(payload.url,payload.body).subscribe((res) => {
+      console.log('-dsadasdasdasdsadsadasda',res)
     });
   }
 }
