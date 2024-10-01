f942951f94e21165701e8d15d70c6e1d2bbe6743
asambasi <asambasi@cisco.com>
2024-10-01 16:00:27 +0530

    update learn

--- src/app/simple-moving/simple-moving.component.ts
+++ src/app/simple-moving/simple-moving.component.ts
@@ -29,22 +29,22 @@ export class SimpleMovingComponent implements OnInit {
   paginationPageSizeSelector = [200, 500, 1000];
   // Column Definitions: Defines the columns to be displayed.
   colDefs: ColDef[] = [
-    { field: "name", sortable: true,resizable:true },
+    { field: "name", sortable: true, resizable: true },
     {
-      headerName: "C.Price",field: "close", sortable: true, valueFormatter: p => (Math.round(p.value * 100) / 100).toLocaleString(),
-      filter: "agNumberColumnFilter",resizable:true,
+      headerName: "C.Price", field: "close", sortable: true, valueFormatter: p => (Math.round(p.value * 100) / 100).toLocaleString(),
+      filter: "agNumberColumnFilter", resizable: true,
       filterParams: {
         numAlwaysVisibleConditions: 2,
         defaultJoinOperator: "OR"
       }
     },
     {
-      headerName: "open %",field: "change_from_open", resizable:true,sortable: true,filter: "agNumberColumnFilter",
+      headerName: "open %", field: "change_from_open", resizable: true, sortable: true, filter: "agNumberColumnFilter",
       filterParams: {
         numAlwaysVisibleConditions: 2,
         defaultJoinOperator: "OR"
       },
-       valueFormatter: p => (Math.round(p.value * 100) / 100).toLocaleString() + '%', cellStyle: function (params) {
+      valueFormatter: p => (Math.round(p.value * 100) / 100).toLocaleString() + '%', cellStyle: function (params) {
         if (params.value > 0) {
           return { backgroundColor: 'green' };
         } else {
@@ -55,21 +55,14 @@ export class SimpleMovingComponent implements OnInit {
     // { field: "preChange", sortable: true, valueFormatter: p => (Math.round(p.value * 100) / 100).toLocaleString() + '%' },
     // { field: "change_from_open_abs", sortable: true, valueFormatter: p => (Math.round(p.value * 100) / 100).toLocaleString() },
     {
-      headerName: "SMA20Close%", field: "sma20closeDiff",resizable:true, sortable: true, valueFormatter: p => (Math.round(p.value * 100) / 100).toLocaleString() + '%', filter: "agNumberColumnFilter",
-      cellStyle: function (params) {
-        if (params.value > 0) {
-          return { backgroundColor: 'green' };
-        } else {
-          return { backgroundColor: 'red' };
-        }
-      },
+      headerName: "20-50%", field: "sma2050Diff", resizable: true, sortable: true, valueFormatter: p => (Math.round(p.value * 100) / 100).toLocaleString() + '%', filter: "agNumberColumnFilter",
       filterParams: {
         numAlwaysVisibleConditions: 2,
         defaultJoinOperator: "OR"
       }
     },
     {
-      headerName: "20 SMA",field: "sma20", resizable:true,sortable: true, valueFormatter: p => (Math.round(p.value * 100) / 100).toLocaleString(),
+      headerName: "20 SMA", field: "sma20", resizable: true, sortable: true, valueFormatter: p => (Math.round(p.value * 100) / 100).toLocaleString(),
       filter: "agNumberColumnFilter",
       filterParams: {
         numAlwaysVisibleConditions: 2,
@@ -77,7 +70,7 @@ export class SimpleMovingComponent implements OnInit {
       }
     },
     {
-      headerName: "50 SMA",field: "sma50",resizable:true, sortable: true, valueFormatter: p => (Math.round(p.value * 100) / 100).toLocaleString(),
+      headerName: "50 SMA", field: "sma50", resizable: true, sortable: true, valueFormatter: p => (Math.round(p.value * 100) / 100).toLocaleString(),
       filter: "agNumberColumnFilter",
       filterParams: {
         numAlwaysVisibleConditions: 2,
@@ -85,14 +78,14 @@ export class SimpleMovingComponent implements OnInit {
       }
     },
     {
-      headerName: "200 SMA",field: "godFather",resizable:true, sortable: true, valueFormatter: p => (Math.round(p.value * 100) / 100).toLocaleString(), filter: "agNumberColumnFilter",
+      headerName: "200 SMA", field: "godFather", resizable: true, sortable: true, valueFormatter: p => (Math.round(p.value * 100) / 100).toLocaleString(), filter: "agNumberColumnFilter",
       filterParams: {
         numAlwaysVisibleConditions: 2,
         defaultJoinOperator: "OR"
       }
     },
     {
-      headerName: "200SMAClose%",field: "godFatherDiffPer",resizable:true, sortable: true, valueFormatter: p => (Math.round(p.value * 100) / 100).toLocaleString() + '%', filter: "agNumberColumnFilter",
+      headerName: "200SMAClose%", field: "godFatherDiffPer", resizable: true, sortable: true, valueFormatter: p => (Math.round(p.value * 100) / 100).toLocaleString() + '%', filter: "agNumberColumnFilter",
       cellStyle: function (params) {
         if (params.value > 0) {
           return { backgroundColor: 'green' };
@@ -105,7 +98,7 @@ export class SimpleMovingComponent implements OnInit {
       }
     },
     {
-      field: "volume", resizable:true,sortable: true,
+      field: "volume", resizable: true, sortable: true,
       filter: "agNumberColumnFilter",
       filterParams: {
         numAlwaysVisibleConditions: 2,
@@ -113,36 +106,37 @@ export class SimpleMovingComponent implements OnInit {
       }
     },
     {
-      headerName: "RSI",field: "RSI",resizable:true, sortable: true, valueFormatter: p => (Math.round(p.value * 100) / 100).toLocaleString(), filter: "agNumberColumnFilter",
+      headerName: "RSI", field: "RSI", resizable: true, sortable: true, valueFormatter: p => (Math.round(p.value * 100) / 100).toLocaleString(), filter: "agNumberColumnFilter",
       filterParams: {
         numAlwaysVisibleConditions: 2,
         defaultJoinOperator: "OR"
       }
     },
     {
-      headerName: "MACD",field: "MACD",resizable:true, sortable: true, valueFormatter: p => (Math.round(p.value * 100) / 100).toLocaleString(), filter: "agNumberColumnFilter",
+      headerName: "MACD", field: "MACD", resizable: true, sortable: true, valueFormatter: p => (Math.round(p.value * 100) / 100).toLocaleString(), filter: "agNumberColumnFilter",
       filterParams: {
         numAlwaysVisibleConditions: 2,
         defaultJoinOperator: "OR"
       }
     },
     {
-      headerName: "52 High",field: "HIGH52",resizable:true, sortable: true, valueFormatter: p => (Math.round(p.value * 100) / 100).toLocaleString(), filter: "agNumberColumnFilter",
+      headerName: "52 High", field: "HIGH52", resizable: true, sortable: true, valueFormatter: p => (Math.round(p.value * 100) / 100).toLocaleString(), filter: "agNumberColumnFilter",
       filterParams: {
         numAlwaysVisibleConditions: 2,
         defaultJoinOperator: "OR"
       }
     },
-    { headerName: "scannerLink",resizable:true,field: 'name', sortable: true,
-    cellRenderer: function(params) {
-      let keyData = params.data.name;
-      let newLink = 
-      `<a style="color:white;" href= https://www.screener.in/company/${keyData}
+    {
+      headerName: "scannerLink", resizable: true, field: 'name', sortable: true,
+      cellRenderer: function (params) {
+        let keyData = params.data.name;
+        let newLink =
+          `<a style="color:white;" href= https://www.screener.in/company/${keyData}
       target="_blank">sceener</a>  |  <a style="color:white;" href= https://in.tradingview.com/chart/6QuU1TVy/?symbol=NSE%3A${keyData}
       target="_blank">chart</a>`;
-      return newLink;
-  }
-}
+        return newLink;
+      }
+    }
     // onCellClicked: (event: CellClickedEvent) =>
     //   window.open( `https://www.screener.in/company/${event.value}/`)
     // },
@@ -154,7 +148,7 @@ export class SimpleMovingComponent implements OnInit {
   gridOptions: GridOptions;
   searchQuery: string = '';
   query: string = '';
-  
+
   constructor(private http: HttpClient, private commonservice: CommonserviceService) {
     this.gridOptions = <GridOptions>{
       // serverSideFiltering: true
@@ -169,13 +163,13 @@ export class SimpleMovingComponent implements OnInit {
   fetchLiveData() {
     this.commonservice.getData.subscribe(data => {
       this.inputValue = data
-      this.getHighLow()
+      this.getStocks()
     });
   }
 
- 
+
   // low,close,sma20,sma50,godFather,volume,RSI,MACD,HIGH52
-  getHighLow() {
+  getStocks() {
     this.allData = []
     this.inputValue.forEach((res) => {
       this.allData.push({
@@ -188,6 +182,8 @@ export class SimpleMovingComponent implements OnInit {
         sma20: res['d'][12],
         sma20closeDiff: (100 * (Number(res['d'][4]) - Number(res['d'][11]))) /
           ((Number(res['d'][11]) + Number(res['d'][4])) / 2),
+        sma2050Diff: ((Math.abs(Number(res['d'][12]) - Number(res['d'][13]))) /
+          ((Number(res['d'][12]) + Number(res['d'][13])) / 2) * 100),
         sma50: res['d'][13],
         godFatherDiffPer:
           (100 * (Number(res['d'][4]) - Number(res['d'][14]))) /
@@ -195,8 +191,11 @@ export class SimpleMovingComponent implements OnInit {
         godFather: res['d'][14],
         volume: res['d'][7],
         RSI: res['d'][27],
-        MACD: Math.abs(Math.abs(res['d'][29]) -  Math.abs(res['d'][30])),
-        HIGH52: res['d'][28]
+        MACD: Math.abs(Math.abs(res['d'][29]) - Math.abs(res['d'][30])),
+        MACDSignal: res['d'][30],
+        HIGH52: res['d'][28],
+        VWAP: res['d'][17],
+        sector: res['d'][18]
       });
     });
     this.rowData = []
@@ -206,78 +205,88 @@ export class SimpleMovingComponent implements OnInit {
 
   }
   // 0: "name",
-// 1: "0pen",
-// 2: "high",
-// 3: "low",
-// 4: "close",
-// 5: "change",
-// 6: "change_abs",
-// 7: "volume",
-// 8: "Value.Traded",
-// 9: "change_from_open",
-// 10: "change_from_open_abs",
-// 11: "SMA20",
-// 12: "SMA20|5",
-// 13: "SMA50|5",
-// 14: "SMA200",
-// 15: "average_volume_10d_calc",
-// 16: "average_volume_30d_calc",
-// 17: "VWAP",
-// 18: "sector",
-// 19: "change_abs|5",
-// 20: "change|5"
-// 21 "BB|1W"
-// 22: "pen|1W",
-// 23: "high|1W",
-// 24: "low|1W",
-// 25: "close|1W"
-// 26: "SMA20|1W",
-// 27: "RSI"
-// 28: "price_52_week_high",
-// 29: "MACD.macd",
-// 30: "MACD.signal"
-   
+  // 1: "0pen",
+  // 2: "high",
+  // 3: "low",
+  // 4: "close",
+  // 5: "change",
+  // 6: "change_abs",
+  // 7: "volume",
+  // 8: "Value.Traded",
+  // 9: "change_from_open",
+  // 10: "change_from_open_abs",
+  // 11: "SMA20",
+  // 12: "SMA20|5",
+  // 13: "SMA50|5",
+  // 14: "SMA200",
+  // 15: "average_volume_10d_calc",
+  // 16: "average_volume_30d_calc",
+  // 17: "VWAP",
+  // 18: "sector",
+  // 19: "change_abs|5",
+  // 20: "change|5"
+  // 21 "BB|1W"
+  // 22: "pen|1W",
+  // 23: "high|1W",
+  // 24: "low|1W",
+  // 25: "close|1W"
+  // 26: "SMA20|1W",
+  // 27: "RSI"
+  // 28: "price_52_week_high",
+  // 29: "MACD.macd",
+  // 30: "MACD.signal"
+
   nearSMA() {
     this.filterData = []
-    // this.allData.forEach((res) => {
-    //   if (res['godFatherDiffPer'] >= -0.5 && res['godFatherDiffPer'] <= 1 && res['volume'] >= 200000) {
-    //   this.filterData.push(res);
-    //   }
-    // });
-        this.allData.forEach((res) => {
+    this.allData.forEach((res) => {
       if (res['low'] < res['godFather'] && res['close'] >= res['godFather'] && res['volume'] >= 200000) {
-      this.filterData.push(res);
+        this.filterData.push(res);
       }
     });
-    
+
     this.rowData = []
     setTimeout(() => {
       this.rowData = this.filterData
     }, 100)
   }
 
+  near2050SMA() {
+    this.filterData = []
+    this.allData.forEach((res) => {
+      if (res['close'] < 2500 && res['close'] >= 300 && res['sma2050Diff'] >= 0.4 && res['sma2050Diff'] <= 2 && res['volume'] >= 300000) {
+        this.filterData.push(res);
+      }
+    });
+
+    this.rowData = []
+    setTimeout(() => {
+      this.rowData = this.filterData
+    }, 100)
+  }
+
+
   onSearchInputChange() {
     if (this.gridOptions.api) {
       this.gridOptions.api.setQuickFilter(this.searchQuery);
     }
   }
-  
+
   querySearch() {
-    if(this.query) {
+    if (this.query) {
       this.filterData = []
-      try { 
-      this.query ? this.query : "res['close'] > 1"
-      this.allData.forEach((res) => {
-      if (eval(`${this.query}`)) {
-      this.filterData.push(res);
-      }
-    });
-    
-    this.rowData = []
-    setTimeout(() => {
-      this.rowData = this.filterData
-    }, 100)
-      } catch(err) {
+      try {
+        this.query ? this.query : "res['close'] > 1"
+        this.allData.forEach((res) => {
+          if (eval(`${this.query}`)) {
+            this.filterData.push(res);
+          }
+        });
+
+        this.rowData = []
+        setTimeout(() => {
+          this.rowData = this.filterData
+        }, 100)
+      } catch (err) {
         alert('invalid Query')
       }
     } else {
@@ -287,7 +296,7 @@ export class SimpleMovingComponent implements OnInit {
 
   onBtnExport() {
     var d = new Date();
-    this.gridOptions.api.exportDataAsCsv({"fileName": `SMA(${d.toLocaleDateString()}).csv`});
+    this.gridOptions.api.exportDataAsCsv({ "fileName": `SMA(${d.toLocaleDateString()}).csv` });
   }
 }
 
