02fc5fb79a6e38edce7b813f073afdb8ca5e2a03
samba425 <asiva325@gmail.com>
2026-03-23 20:48:21 +0530

    updated new logics

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: false,
-  // baseUrl:'https://livetradeapp1.onrender.com/'
-  baseUrl: 'http://localhost:5001/'
+  baseUrl:'https://livetradeapp1.onrender.com/'
+  // baseUrl: 'http://localhost:5001/'
 };
 
 /*
